d7e4b68ed682238e1a9f820cfd8d77423857c64a
Rafael Pezzuto <rafael.pezzuto@gmail.com>
2016-06-02 14:09:58 -0300

    added problems 27-30

--- cap2.py
+++ cap2.py
@@ -1,4 +1,4 @@
-# Problem 7
+# problem 7
 def max (lista):
 	if (len (lista) > 0):
 		maxa = lista[0]
@@ -7,7 +7,7 @@ def max (lista):
 				maxa = lista[i]
 		return maxa
 
-# Problem 7
+# problem 7
 def min (lista):
 	if (len (lista) > 0):
 		mina = lista[0]
@@ -16,7 +16,7 @@ def min (lista):
 				mina = lista[i]
 		return mina
 
-# Problem 8
+# problem 8
 def cumulative_sum (lista):
 	if (len (lista) > 0):
 		sum = lista[0]
@@ -26,7 +26,7 @@ def cumulative_sum (lista):
 			cumsum.append (sum)
 		return cumsum
 
-# Problem 9
+# problem 9
 def cumulative_product (lista):
 	if (len (lista) > 0):
 		sum = lista[0]
@@ -36,7 +36,7 @@ def cumulative_product (lista):
 			cumsum.append (sum)
 		return cumsum
 
-# Problem 10
+# problem 10
 # def unique (lista):
 # 	if (len (lista) > 0):
 # 		uniquelist = [lista[0]]
@@ -45,7 +45,7 @@ def cumulative_product (lista):
 # 				uniquelist.append (lista[i])
 # 		return uniquelist
 
-# Problem 14
+# problem 14
 # def unique (lista, key):
 # 	if (len (lista) > 0):
 # 		uniquelist = [key (lista[0])]
@@ -54,7 +54,7 @@ def cumulative_product (lista):
 # 				uniquelist.append (key (lista[i]))
 # 		return uniquelist
 
-# Problem 15
+# problem 15
 def unique (lista, key):
 	if (len (lista) > 0):
 		uniquelist = {key (lista[0])}
@@ -62,7 +62,7 @@ def unique (lista, key):
 			uniquelist.add (key (lista[i]))
 		return uniquelist
 
-# Problem 11
+# problem 11
 def dups (lista):
 	if (len (lista) > 0):
 		uniquelist = [lista[0]]
@@ -74,7 +74,7 @@ def dups (lista):
 				dupslist.append (lista[i])
 		return dupslist
 
-# Problem 12
+# problem 12
 def group (lista, k):
 	if (len (lista) > 0 and k > 0):
 		groupslist = []
@@ -82,21 +82,39 @@ def group (lista, k):
 			groupslist.append (lista[i:i + k])
 		return groupslist
 
-# Problem 13
+# problem 13
 def lensort (lista):
 		return sorted (lista, key = lambda x : len (x))
 
-# Problem 16
+# problem 16
 def extsort (lista):
-	return sorted (lista, key = lambda x : x.split('.')[1])
+	return sorted (lista, key = lambda x : x.split ('.')[1])
 
-# Problem 24
+# problem 24
 def zip (lista1, lista2):
-	return [(x,y) for indx, x in enumerate(lista1) for indy, y in enumerate(lista2) if indx==indy]
+	return [(x,y) for indx, x in enumerate (lista1) for indy, y in enumerate (lista2) if indx == indy]
 
+# problem 25
 def map (funcao, lista):
 	return [funcao(x) for x in lista]
 
-def filter(filtro, lista):
-	return [x for x in lista if filtro(x)]
+# problem 26
+def filter (filtro, lista):
+	return [x for x in lista if filtro (x)]
 
+# problem 27
+def triplets (n):
+	return [(x, y, z) for x in range (1, n) for y in range (x, n) for z in range (y, n) if x + y == z]
+
+# problem 28: preciso implementar sem usar a funcao enumerate do python
+def myenumerate (lista):
+	return [(indice, valor) for indice, valor in enumerate (lista)]
+
+# problem 29
+def array (linhas, colunas):
+	return [[None for x in range (linhas)] for y in range (colunas)]
+
+# problem 30
+def parse_csv (arquivo):
+	dados = open (arquivo).readlines()
+	return [x.strip ('\n'). split(',') for x in dados]
